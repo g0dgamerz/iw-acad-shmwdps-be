62c4dee1250401356c738b281d4cc05b7e1779e9
g0dgamerz <jideshvaidya@gmail.com>
2021-02-11 22:07:01 +0545

    model of lab changed

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import HospitalDetail, Room,Bed,Lab,Appointment,Bill,Disease,Symptoms
+
+admin.site.register(HospitalDetail)
+admin.site.register(Room)
+admin.site.register(Bed)
+admin.site.register(Lab)
+admin.site.register(Appointment)
+admin.site.register(Bill)
+admin.site.register(Disease)
+admin.site.register(Symptoms)
